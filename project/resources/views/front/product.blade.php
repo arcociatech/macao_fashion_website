f3778593a3047804584ff0776718465fe8aac7fa
Afrasiyab Haider <afrasiyab555@gmail.com>
2021-05-25 02:27:28 +0500

    Update product.blade.php

--- project/resources/views/front/product.blade.php
+++ project/resources/views/front/product.blade.php
@@ -223,12 +223,12 @@
                       @php
                       $is_first = true;
                       @endphp
-                      {{ dd($productt->color,$productt->color_image) }}
+                      {{-- {{ dd($productt->color,$productt->color_image) }} --}}
                       @foreach($productt->color_image as $key => $data1)
                       {{-- {{ $is_first ? 'active' : '' }} --}}
                         <li class="{{ $is_first ? 'active' : '' }} text-center color-select">
                             {{-- <span class="box" data-color="{{ $productt->color[$key] }}" style="background-color: {{ $productt->color[$key] }}"></span> --}}
-                                <img class="box" data-color="{{ $productt->color[$key] }}" src="{{ asset($productt->color_image[$key]) }}" alt="{{ $productt->color[$key] }} image"width="60px" height="60px" style="border-radius: 30px; width: 60px; height:60px">
+                                <img class="box" data-color="{{ $productt->color[$key] ?? '' }}" src="{{ asset($productt->color_image[$key]) }}" alt="{{ $productt->color[$key] }} image"width="60px" height="60px" style="border-radius: 30px; width: 60px; height:60px">
                                 <br>
                                 {{ $productt->color[$key] }}
                         </li>
